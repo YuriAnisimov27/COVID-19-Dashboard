282672e06d650940962559aa0b39cf3bd8c9d6e8
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-12-19 22:21:31 +0300

    refactor: remove listenets to LIst Component

--- src/modules/components/CountryList.component.js
+++ src/modules/components/CountryList.component.js
@@ -104,4 +104,50 @@ export default class CountryList {
         });
     });
   }
+
+  sortList() {
+    this.bar = 'Hello World';
+    const casesBtn = document.querySelector('.sort-cases');
+    const recoveredBtn = document.querySelector('.sort-recovered');
+    const deathsBtn = document.querySelector('.sort-deaths');
+    const sortIcon = document.querySelector('.sort-icon');
+    let data = storage('Global').Countries;
+    let position = true;
+
+    casesBtn.addEventListener('click', () => {
+      if (position) {
+        data = data.sort((a, b) => a.TotalConfirmed - b.TotalConfirmed);
+        sortIcon.textContent = 'keyboard_arrow_down';
+      } else {
+        data = data.sort((a, b) => b.TotalConfirmed - a.TotalConfirmed);
+        sortIcon.textContent = 'keyboard_arrow_up';
+      }
+      new CountryList().allCountries(data);
+      position = !position;
+    });
+
+    recoveredBtn.addEventListener('click', () => {
+      if (position) {
+        data = data.sort((a, b) => a.TotalRecovered - b.TotalRecovered);
+        sortIcon.textContent = 'keyboard_arrow_down';
+      } else {
+        data = data.sort((a, b) => b.TotalRecovered - a.TotalRecovered);
+        sortIcon.textContent = 'keyboard_arrow_up';
+      }
+      new CountryList().allCountries(data);
+      position = !position;
+    });
+
+    deathsBtn.addEventListener('click', () => {
+      if (position) {
+        data = data.sort((a, b) => a.TotalDeaths - b.TotalDeaths);
+        sortIcon.textContent = 'keyboard_arrow_down';
+      } else {
+        data = data.sort((a, b) => b.TotalDeaths - a.TotalDeaths);
+        sortIcon.textContent = 'keyboard_arrow_up';
+      }
+      new CountryList().allCountries(data);
+      position = !position;
+    });
+  }
 }
